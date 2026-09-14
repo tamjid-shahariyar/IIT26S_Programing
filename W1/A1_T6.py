2094a4585980b2e54c007cae47d6ded52acf4abf
Tamjid Shahariyar <tamjidshahhariyar@gmail.com>
2026-09-14 12:47:26 +0300

    Completed A1_T6 task

---
+++ W1/A1_T6.py
@@ -0,0 +1,8 @@
+Feed = input("Insert an integer: ")
+
+Value = int(Feed)
+
+Remainder = Value % 2
+
+print("Value is", Value)
+print("The remainder is", Remainder, "when", Value, "is divided by 2.")
